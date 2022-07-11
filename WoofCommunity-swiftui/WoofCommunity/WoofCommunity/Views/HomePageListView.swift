e4f7218f7939ed8a455e335d8c109bbe405a7c5d
kathleendiep <kathleendiep@gmail.com>
2022-07-11 14:24:57 -0700

    mvc

---
+++ WoofCommunity-swiftui/WoofCommunity/WoofCommunity/Views/HomePageListView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageListView.swift
+//  WoofCommunity
+//
+//  Created by Kathleen Diep on 7/11/22.
+//
+
+import SwiftUI
+
+struct HomePageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePageListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageListView()
+    }
+}
--- WoofCommunity-swiftui/WoofCommunity/WoofCommunity/Views/WoofCommunityApp.swift
+++ WoofCommunity-swiftui/WoofCommunity/WoofCommunity/Views/WoofCommunityApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WoofCommunityApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomePageListView()
         }
     }
 }
