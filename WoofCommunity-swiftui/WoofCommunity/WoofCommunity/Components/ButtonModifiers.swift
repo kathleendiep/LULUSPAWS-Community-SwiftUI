480d8dfc70ecd9d1f81f6fd0c48dd8290db92ed6
kathleendiep <kathleendiep@gmail.com>
2022-07-18 16:13:54 -0700

    button modifier

---
+++ WoofCommunity-swiftui/WoofCommunity/WoofCommunity/Components/ButtonModifiers.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  WoofCommunity
+//
+//  Created by Kathleen Diep on 7/18/22.
+//
+
+import SwiftUI
+
+struct ButtonModifiers: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .frame(height: 20)
+            .padding()
+            .foregroundColor(.white)
+            .font(.system(size: 14, weight: .bold))
+            .background(Color.black)
+            .cornerRadius(5.0)
+
+    }
+}
+// to add later
+//Button(action: {}){
+//    Text("Sign In").font(.title)
+//        .modifier(ButtonModifiers())
+//
+//}
