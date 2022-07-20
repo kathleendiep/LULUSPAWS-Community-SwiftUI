f62b8c5cbea8138b1f4042cb922c6fb5ed65000c
kathleendiep <kathleendiep@gmail.com>
2022-07-20 14:23:31 -0700

    postviewmodel

---
+++ WoofCommunity-swiftui/WoofCommunity/WoofCommunity/ViewModels/PostViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PostViewModel.swift
+//  WoofCommunity
+//
+//  Created by Kathleen Diep on 7/20/22.
+//
+
+import Foundation
+
+class PostViewModel {
+    
+    static var Posts = SignInViewModel.storeRoot.collection("posts")
+    
+}
